a4eb3ef11c9128868f20f75d87d9510d835fe1f8
Hasan-aga <careerforma@gmail.com>
2022-07-05 16:40:14 +0300

    learn loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -14,21 +14,25 @@
 // names[3] = "ibrahim";
 // console.log(names, names[0], names.length);
 
-const objectName = {
-  key1: "value",
-  key2: 100,
-  key5: function () {
-    console.log(`the value of key1 is ${this.key1}`);
-  },
+// const objectName = {
+//   key1: "value",
+//   key2: 100,
+//   key5: function () {
+//     console.log(`the value of key1 is ${this.key1}`);
+//   },
 
-  key6: function () {
-    this.key7 = this.key1 + this.key2;
-    return this.key7;
-  },
-};
+//   key6: function () {
+//     this.key7 = this.key1 + this.key2;
+//     return this.key7;
+//   },
+// };
 
-objectName.key3 = "another";
-objectName["key4"] = "whatever";
+// objectName.key3 = "another";
+// objectName["key4"] = "whatever";
 
-objectName.key6();
-console.log(objectName.key7);
+// objectName.key6();
+// console.log(objectName.key7);
+
+for (let index = 0; index < 10; index++) {
+  console.log(`I am on the ${index} index`);
+}
