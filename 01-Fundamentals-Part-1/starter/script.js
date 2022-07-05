6d817e9b1158a0bf10fd02ca0a62a06ffea29c24
Hasan-aga <careerforma@gmail.com>
2022-07-05 11:16:29 +0300

    more learning

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,9 +3,21 @@ let firstname = "hasan",
 
 const info = `Hello, 
 my name is ${firstname} ${lastname}`;
-console.log(info);
 
 // type coercion
 let n = 1 + "1";
 n -= 1;
-console.log(n);
+
+let day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("monday");
+    break;
+
+  case "friday":
+    console.log("get ready to pray");
+  default:
+    console.log("what day is it?");
+    break;
+}
