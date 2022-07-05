e98599d5a390c404d9908b25d36b5d04142b1488
Hasan-aga <careerforma@gmail.com>
2022-07-05 16:11:23 +0300

    learn objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,5 +7,28 @@
 
 // console.log(greet("hasan", "ali"));
 
-let greet = (firstname, lastname) => `hello ${firstname} ${lastname}`;
-console.log(greet("hasan", "ali"));
+// let greet = (firstname, lastname) => `hello ${firstname} ${lastname}`;
+// console.log(greet("hasan", "ali"));
+
+// const names = ["hasan", "ali"];
+// names[3] = "ibrahim";
+// console.log(names, names[0], names.length);
+
+const objectName = {
+  key1: "value",
+  key2: 100,
+  key5: function () {
+    console.log(`the value of key1 is ${this.key1}`);
+  },
+
+  key6: function () {
+    this.key7 = this.key1 + this.key2;
+    return this.key7;
+  },
+};
+
+objectName.key3 = "another";
+objectName["key4"] = "whatever";
+
+objectName.key6();
+console.log(objectName.key7);
