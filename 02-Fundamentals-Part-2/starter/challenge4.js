eafa4925ba55f8c13b30cbfe81cd700e6262ba4f
Hasan-aga <careerforma@gmail.com>
2022-07-05 16:55:52 +0300

    finish challenge 4

--- 02-Fundamentals-Part-2/starter/challenge4.js
+++ 02-Fundamentals-Part-2/starter/challenge4.js
@@ -5,3 +5,21 @@ let totals = [];
 function calcTip(bill) {
   return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
 }
+
+for (let index = 0; index < bills.length; index++) {
+  const bill = bills[index];
+  const tip = calcTip(bill);
+  const total = tip + bill;
+  tips.push(tip);
+  totals.push(total);
+}
+
+const calcAvg = function (arr) {
+  let sum = 0;
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAvg([1, 2, 3]));
