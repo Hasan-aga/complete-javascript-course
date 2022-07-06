ba721043bae365616eed0b2dbd24b40f73a71052
Hasan-aga <careerforma@gmail.com>
2022-07-06 11:30:11 +0300

    guess game / access and manipulate text

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+
+console.log(document.querySelector(".result-hint").textContent);
+document.querySelector(".result-hint").textContent = "Start guessing...";
+
+document.querySelector(".display").textContent = 1;
+document.querySelector(".result-score").textContent = `Score: 1`;
