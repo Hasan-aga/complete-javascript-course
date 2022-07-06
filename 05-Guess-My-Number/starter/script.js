f98fc5cf93df718cf23112130962186311715ad0
Hasan-aga <careerforma@gmail.com>
2022-07-06 12:23:25 +0300

    guess game / handle scoring logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,25 +10,32 @@
 const writeToUser = function (message) {
   document.querySelector(".result-hint").textContent = message;
 };
+let score = 20;
 
-const logScore = (score) =>
-  (document.querySelector(".result-score").textContent = `Score: ${score}`);
+const logScore = (playerCondition) =>
+  (document.querySelector(".result-score").textContent =
+    playerCondition !== "lost" ? `Score: ${score--}` : `Score: 0`);
+
+logScore();
 
 const correctNumber = Math.trunc(Math.random() * 20) + 1;
-
-console.log(correctNumber);
 
 const check = document.querySelector(".button-check");
 const userInput = document.querySelector(".user-input");
 check.addEventListener("click", function () {
   const guess = Number(userInput.value);
+  if (score > 0) {
+    if (!guess) {
+      writeToUser("Pick a number!");
+    } else if (guess === correctNumber) {
+      writeToUser("Correct!!!");
+    } else if (guess < correctNumber) {
+      writeToUser("Too low");
+    } else writeToUser("Too high");
 
-  console.log(guess, Boolean(guess));
-  if (!guess) {
-    writeToUser("Pick a number!");
-  } else if (guess === correctNumber) {
-    writeToUser("Correct!!!");
-  } else if (guess < correctNumber) {
-    writeToUser("Too low");
-  } else writeToUser("Too high");
+    logScore();
+  } else {
+    writeToUser("You LOST!");
+    logScore("lost");
+  }
 });
