741fb9b9e208eb923f6d460da28f8bf0569e1de4
Hasan-aga <careerforma@gmail.com>
2022-07-06 21:26:45 +0300

    modal / refactor code use functions with descriptive names

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,18 +1,28 @@
-'use strict';
+"use strict";
 
-const showButtons = document.querySelectorAll('.show-modal');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const closeButton = document.querySelector('.close-modal');
+const showButtons = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeButton = document.querySelector(".close-modal");
 
-showButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+showButtons.forEach((button) => {
+  button.addEventListener("click", openModal);
 });
+function hideModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
 
-closeButton.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+closeButton.addEventListener("click", hideModal);
+overlay.addEventListener("click", hideModal);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    hideModal();
+  }
 });
