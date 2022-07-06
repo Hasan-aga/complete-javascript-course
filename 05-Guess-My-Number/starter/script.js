0604d8264a341dd2b3e0fbee0eeef4faebd7e418
Hasan-aga <careerforma@gmail.com>
2022-07-06 11:39:55 +0300

    guess game /  read from input field

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,15 @@
 "use strict";
 
-console.log(document.querySelector(".result-hint").textContent);
-document.querySelector(".result-hint").textContent = "Start guessing...";
+// console.log(document.querySelector(".result-hint").textContent);
+// document.querySelector(".result-hint").textContent = "Start guessing...";
 
-document.querySelector(".display").textContent = 1;
-document.querySelector(".result-score").textContent = `Score: 1`;
+// document.querySelector(".display").textContent = 1;
+// document.querySelector(".result-score").textContent = `Score: 1`;
+
+// console.log(document.querySelector(".user-input").value);
+
+const check = document.querySelector(".button-check");
+const userInput = document.querySelector(".user-input");
+check.addEventListener("click", function () {
+  console.log(userInput.value);
+});
