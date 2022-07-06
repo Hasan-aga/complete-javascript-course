7e62dffd62541e7f8063e035540bac8e029bdbd9
Hasan-aga <careerforma@gmail.com>
2022-07-06 12:20:04 +0300

    guess game / implement logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,12 +4,31 @@
 // document.querySelector(".result-hint").textContent = "Start guessing...";
 
 // document.querySelector(".display").textContent = 1;
-// document.querySelector(".result-score").textContent = `Score: 1`;
 
 // console.log(document.querySelector(".user-input").value);
+
+const writeToUser = function (message) {
+  document.querySelector(".result-hint").textContent = message;
+};
+
+const logScore = (score) =>
+  (document.querySelector(".result-score").textContent = `Score: ${score}`);
+
+const correctNumber = Math.trunc(Math.random() * 20) + 1;
+
+console.log(correctNumber);
 
 const check = document.querySelector(".button-check");
 const userInput = document.querySelector(".user-input");
 check.addEventListener("click", function () {
-  console.log(userInput.value);
+  const guess = Number(userInput.value);
+
+  console.log(guess, Boolean(guess));
+  if (!guess) {
+    writeToUser("Pick a number!");
+  } else if (guess === correctNumber) {
+    writeToUser("Correct!!!");
+  } else if (guess < correctNumber) {
+    writeToUser("Too low");
+  } else writeToUser("Too high");
 });
