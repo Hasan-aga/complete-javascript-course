5d6688daf0031687d33a57aaace5a7377ef15777
Hasan-aga <careerforma@gmail.com>
2022-07-06 21:16:56 +0300

    modal / finish

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const showButtons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+
+showButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+closeButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
