480e6d13e2a9af9b38e33e8d64cbccb53bca9a92
Hasan-aga <careerforma@gmail.com>
2022-07-07 18:42:21 +0300

    dice game / style winner

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,49 @@
 'use strict';
 
+// ///////////////////////////////////////////////////////
+// switch theme
+// ///////////////////////////////////////////////////////
+const lightColor = '#9b5de5';
+const darkColor = '#003566';
+
+const lightActivePlayerColor = '#ff99c8';
+const darkActivePlayerColor = '#ffc300';
+
+const lightStandbyPlayerColor = '#ffd6e9';
+const darkStandbyPlayerColor = '#fff3cc';
+
+const lightGlow = 'rgba(252, 246, 189, 0.747)';
+const darkGlow = 'rgba(255, 242, 0, 0.81)';
+
+const darkBg =
+  'linear-gradient(to right, #001d3d, #113865, #245591, #3674bf, #4895ef)';
+const lightBg =
+  'linear-gradient(to right, #d0f4de, #b0f0ee, #a4e6ff, #bbd6ff, #e4c1f9)';
+
+const checkbox = document.getElementById('checkbox');
+
+function setProp(propName, propValue) {
+  document.documentElement.style.setProperty(propName, propValue);
+}
+
+checkbox.addEventListener('change', e => {
+  if (e.currentTarget.checked) {
+    setProp('--color', lightColor);
+    setProp('--bg', lightBg);
+    setProp('--active-player-color', lightActivePlayerColor);
+    setProp('--standby-player-color', lightStandbyPlayerColor);
+    setProp('--glow', lightGlow);
+  } else {
+    setProp('--color', darkColor);
+    setProp('--bg', darkBg);
+    setProp('--active-player-color', darkActivePlayerColor);
+    setProp('--standby-player-color', darkStandbyPlayerColor);
+    setProp('--glow', darkGlow);
+  }
+});
+// ///////////////////////////////////////////////////////////////
 // helper functions
+// ///////////////////////////////////////////////////////////////
 function displayCurrentScoreOfPlayer(player, score) {
   document.querySelector(`.${player} .current-score`).textContent = score;
 }
@@ -28,6 +71,7 @@ function resetCurrentScore() {
 }
 
 function resetGame() {
+  document.getElementById(currentActivePlayerID).classList.remove('winner');
   displayCurrentScoreOfPlayer('player1', 0);
   displayCurrentScoreOfPlayer('player0', 0);
   score0Element.textContent = 0;
@@ -88,8 +132,8 @@ holdButton.addEventListener('click', function () {
   resetCurrentScore();
 
   //   if total score > 100 player wins
-  if (totalScores[currentActivePlayerID] >= 100) {
-    console.log('win');
+  if (totalScores[currentActivePlayerID] >= 10) {
+    document.getElementById(currentActivePlayerID).classList.add('winner');
   } else {
     // switch player
     switchActivePlayer();
@@ -99,46 +143,4 @@ holdButton.addEventListener('click', function () {
 // reset game
 newGameButton.addEventListener('click', function () {
   resetGame();
-});
-
-// ///////////////////////////////////////////////////////
-// switch theme
-// ///////////////////////////////////////////////////////
-const lightColor = '#9b5de5';
-const darkColor = '#003566';
-
-const lightActivePlayerColor = '#ff99c8';
-const darkActivePlayerColor = '#ffc300';
-
-const lightStandbyPlayerColor = '#ffd6e9';
-const darkStandbyPlayerColor = '#fff3cc';
-
-const lightGlow = 'rgba(252, 246, 189, 0.747)';
-const darkGlow = 'rgba(255, 242, 0, 0.81)';
-
-const darkBg =
-  'linear-gradient(to right, #001d3d, #113865, #245591, #3674bf, #4895ef)';
-const lightBg =
-  'linear-gradient(to right, #d0f4de, #b0f0ee, #a4e6ff, #bbd6ff, #e4c1f9)';
-
-const checkbox = document.getElementById('checkbox');
-
-function setProp(propName, propValue) {
-  document.documentElement.style.setProperty(propName, propValue);
-}
-
-checkbox.addEventListener('change', e => {
-  if (e.currentTarget.checked) {
-    setProp('--color', lightColor);
-    setProp('--bg', lightBg);
-    setProp('--active-player-color', lightActivePlayerColor);
-    setProp('--standby-player-color', lightStandbyPlayerColor);
-    setProp('--glow', lightGlow);
-  } else {
-    setProp('--color', darkColor);
-    setProp('--bg', darkBg);
-    setProp('--active-player-color', darkActivePlayerColor);
-    setProp('--standby-player-color', darkStandbyPlayerColor);
-    setProp('--glow', darkGlow);
-  }
 });
