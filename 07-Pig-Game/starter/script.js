1ca04c692e982421c675037574427762abf97f7f
Hasan-aga <careerforma@gmail.com>
2022-07-07 14:45:42 +0300

    dice / working on changing theme

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,6 +2,15 @@
 
 // switch theme
 const lightColor = '#9b5de5';
+const darkColor = '#003566';
+
+const lightActivePlayerColor = '#ff99c8';
+const darkActivePlayerColor = '#ffc300';
+
+const darkBg =
+  'linear-gradient(to right, #001d3d, #113865, #245591, #3674bf, #4895ef)';
+const lightBg =
+  'linear-gradient(to right, #d0f4de, #b0f0ee, #a4e6ff, #bbd6ff, #e4c1f9)';
 
 const checkbox = document.getElementById('checkbox');
 
@@ -12,6 +21,9 @@ function setProp(propName, propValue) {
 checkbox.addEventListener('change', e => {
   if (e.currentTarget.checked) {
     setProp('--color', lightColor);
+    setProp('--bg', lightBg);
   } else {
+    setProp('--color', darkColor);
+    setProp('--bg', darkBg);
   }
 });
