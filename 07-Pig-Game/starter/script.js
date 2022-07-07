662a8c50beb4c9e536599fdb3987b9f137bc0494
Hasan-aga <careerforma@gmail.com>
2022-07-07 14:39:01 +0300

    dice / change color on checkbox

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,8 +1,17 @@
 'use strict';
 
 // switch theme
+const lightColor = '#9b5de5';
+
 const checkbox = document.getElementById('checkbox');
 
-checkbox.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
+function setProp(propName, propValue) {
+  document.documentElement.style.setProperty(propName, propValue);
+}
+
+checkbox.addEventListener('change', e => {
+  if (e.currentTarget.checked) {
+    setProp('--color', lightColor);
+  } else {
+  }
 });
