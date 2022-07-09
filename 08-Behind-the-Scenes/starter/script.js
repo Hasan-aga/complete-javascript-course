026129b4f31df34ef20f0b73ff0d36646448ecc8
Hasan-aga <careerforma@gmail.com>
2022-07-09 17:07:05 +0300

    learn functions argument

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+// console.log(this);
+
+// const username = 'hasan';
+
+// function greetUser() {
+//   const username = 'ali';
+//   const arr = () => {};
+
+//   arr();
+// }
+
+// greetUser();
+
+function add(first, second, third) {
+  let sum = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    sum += arguments[index];
+  }
+
+  return sum;
+}
+
+console.log(add(1, 2, 3));
