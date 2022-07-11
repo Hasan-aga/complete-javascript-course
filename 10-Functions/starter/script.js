371c243250a18cff5b8a793b6c61272012161704
Hasan-aga <careerforma@gmail.com>
2022-07-11 15:30:23 +0300

    demo: function methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -80,59 +80,59 @@
 // greetArrow('selam')('Hasan Ali');
 
 //the call and apply methods
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(passengerName, flightNum) {
-    console.log(
-      `${passengerName} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({
-      flight: `${this.airline}${this.iataCode}${flightNum}`,
-      passengerName,
-    });
-  },
-};
-lufthansa.book('Hasan Ali', 123);
-lufthansa.book('John Ali', 103);
-console.log(lufthansa);
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(passengerName, flightNum) {
+//     console.log(
+//       `${passengerName} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({
+//       flight: `${this.airline}${this.iataCode}${flightNum}`,
+//       passengerName,
+//     });
+//   },
+// };
+// lufthansa.book('Hasan Ali', 123);
+// lufthansa.book('John Ali', 103);
+// console.log(lufthansa);
 
-const eurowings = {
-  airline: 'eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+// const eurowings = {
+//   airline: 'eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
 
-const book = lufthansa.book;
+// const book = lufthansa.book;
 
-// book('hawawa', 20);
-book.call(eurowings, 'hawawa', '20');
-book.apply(eurowings, ['kawasaki', 110]);
+// // book('hawawa', 20);
+// book.call(eurowings, 'hawawa', '20');
+// book.apply(eurowings, ['kawasaki', 110]);
 
-// bind method
-const bookEW = book.bind(eurowings);
-bookEW('jack sparrow', 19);
+// // bind method
+// const bookEW = book.bind(eurowings);
+// bookEW('jack sparrow', 19);
 
-// with event listner
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  this.planes++;
-  console.log(`${this.airline} has ${this.planes} plane`);
-};
+// // with event listner
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   this.planes++;
+//   console.log(`${this.airline} has ${this.planes} plane`);
+// };
 
 // will not work since 'this' points to DOM element
 // document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
 
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
-//   partial application of bind
-function greet(greeting, fullname) {
-  console.log(`${greeting} ${fullname}`);
-}
+// //   partial application of bind
+// function greet(greeting, fullname) {
+//   console.log(`${greeting} ${fullname}`);
+// }
 
-const greetHello = greet.bind(null, 'hello');
+// const greetHello = greet.bind(null, 'hello');
 
-greetHello('hasan');
+// greetHello('hasan');
