04b11d77984c5c124b1e116f7b5598c3485e9db1
Hasan-aga <careerforma@gmail.com>
2022-07-11 11:23:40 +0300

    demo: passing arg to functions reference vs primitive

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,3 +24,24 @@
 // createBooking('LH3', undefined, 2500);
 
 // console.log(bookings);
+
+// const flight = 'LH31';
+// const passenger = {
+//   fullName: 'Hasan Ali',
+//   Age: '30',
+// };
+
+// function checkIn(flight, { ...passenger }) {
+//   flight = '0000';
+//   passenger.fullName = 'Mr.' + passenger.fullName;
+
+//   if (passenger.Age > 17) {
+//     console.log('Welcome aboard ' + passenger.fullName);
+//   } else {
+//     console.log('Go home');
+//   }
+// }
+
+// checkIn(flight, passenger);
+// console.log(flight);
+// console.log({ ...passenger });
