108fd7d996572126f32385572db0c52483ea39d6
Hasan-aga <careerforma@gmail.com>
2022-07-11 10:52:35 +0300

    demo: default params

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// let bookings = [];
+// function createBooking(
+//   flightNum,
+//   passengerNum = 1,
+//   price = 100 * passengerNum
+// ) {
+//   //  old way
+//   //   passengerNum ||= 1;
+//   //   price ||= 100;
+
+//   const booking = {
+//     flightNum,
+//     passengerNum,
+//     price,
+//   };
+
+//   bookings.push(booking);
+// }
+
+// createBooking('LH3');
+// createBooking('LH3', 2);
+// createBooking('LH3', undefined, 2500);
+
+// console.log(bookings);
