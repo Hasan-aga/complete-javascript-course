4f5686dc2806d0d66190bdd102b9cc46e317dae6
Hasan-aga <careerforma@gmail.com>
2022-07-12 17:03:20 +0300

    arrays: challenge 3

--- 11-Arrays-Bankist/starter/challenge2.js
+++ 11-Arrays-Bankist/starter/challenge2.js
@@ -1,16 +1,28 @@
-'use strict';
+"use strict";
 
-function calculateAverageHumanAge(dogAges) {
-  const humanAges = dogAges
-    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 4 * dogAge + 16))
-    .filter(humanAge => humanAge >= 18);
+// function calculateAverageHumanAge(dogAges) {
+//   const humanAges = dogAges
+//     .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 4 * dogAge + 16))
+//     .filter(humanAge => humanAge >= 18);
 
-  const average =
-    humanAges.reduce(function (acc, age) {
-      return acc + age;
-    }, 0) / humanAges.length;
+//   const average =
+//     humanAges.reduce(function (acc, age) {
+//       return acc + age;
+//     }, 0) / humanAges.length;
 
-  return { humanAges, average };
-}
+//   return { humanAges, average };
+// }
 
-console.log(calculateAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calculateAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+
+const dogAges = [5, 2, 4, 1, 15, 8, 3];
+
+const calculateAverageHumanAge = (dogAges) =>
+  dogAges
+    .map((dogAge) => (dogAge <= 2 ? 2 * dogAge : 4 * dogAge + 16))
+    .filter((humanAge) => humanAge >= 18)
+    .reduce(function (acc, age, i, arr) {
+      return acc + age / arr.length;
+    }, 0);
+
+console.log(calculateAverageHumanAge(dogAges));
