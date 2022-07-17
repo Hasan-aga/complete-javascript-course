90bd155b4247977c04d0d3e308fba04bb37e737b
Hasan-aga <careerforma@gmail.com>
2022-07-17 21:06:10 +0300

    oop: constructor demo

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const Person = function (firstName, age) {
+  (this.firstName = firstName), (this.age = age);
+  this.calcBirthYear = () => new Date().getFullYear() - this.age;
+};
+
+const hasan = new Person('Hasan', 30);
+
+const matilda = new Person('Matilda', 19);
+
+console.log(hasan.calcBirthYear());
