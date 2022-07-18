74eeb988ac1fc7b8e2f1c69fedfb61cd90403433
Hasan-aga <careerforma@gmail.com>
2022-07-18 17:43:15 +0300

    oop: prototypal inheritance

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -2,11 +2,29 @@
 
 const Person = function (firstName, age) {
   (this.firstName = firstName), (this.age = age);
-  this.calcBirthYear = () => new Date().getFullYear() - this.age;
 };
+
+Person.prototype = {
+  calcBirthYear: function () {
+    return new Date().getFullYear() - this.age;
+  },
+};
+
+Person.prototype.job = 'programmer';
 
 const hasan = new Person('Hasan', 30);
 
 const matilda = new Person('Matilda', 19);
 
-console.log(hasan.calcBirthYear());
+// prototypes
+
+console.dir(Person.prototype.constructor);
+
+// prototypal inheritance
+
+const arr = [1, 2, 3, 1, 2, 3];
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
