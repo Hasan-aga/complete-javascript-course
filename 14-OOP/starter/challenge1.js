f7a5647b3ba4e72aa1b74a134ab8ec35d4298596
Hasan-aga <careerforma@gmail.com>
2022-07-19 18:35:01 +0300

    ch2

--- 14-OOP/starter/challenge1.js
+++ 14-OOP/starter/challenge1.js
@@ -1,47 +1,38 @@
 'use strict';
 
-const Car = function (make, speed) {
-  this.make = make.toUpperCase();
-  this.speed = speed;
-};
+class Car {
+  constructor(make, speed) {
+    this.make = make.toUpperCase();
+    this.speed = speed;
+  }
 
-Car.prototype.accelarate = function () {
-  this.speed += 10;
-};
+  accelarate() {
+    this.speed += 10;
+  }
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-};
+  brake() {
+    this.speed -= 5;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
 
 const bmw = new Car('bmw', 120);
 const mercedes = new Car('mercedes', 95);
-// console.log(bmw);
-// bmw.accelarate();
-// console.log(bmw);
-// bmw.brake();
-// console.log(bmw);
+console.log(bmw);
+bmw.accelarate();
+console.log(bmw);
+bmw.brake();
+console.log(bmw);
+
+console.log(bmw.speedUS);
+bmw.speedUS = 60;
+console.log(bmw.speedUS);
 
 // ch 2
-
-const ev = Object.create(Car);
-
-ev.charge = 23;
-ev.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-ev.accelarate = function () {
-  this.speed += 20;
-  this.charge -= 1;
-  console.log(
-    `${this.make} is going at ${this.speed} with ${this.charge}% charged battery`
-  );
-};
-
-ev.make = 'tesla';
-ev.speed = 120;
-ev.chargeBattery(90);
-ev.accelarate();
-ev.accelarate();
-ev.accelarate();
-ev.accelarate();
-ev.accelarate();
