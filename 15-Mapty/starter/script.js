53919aca5cac0ebef5dac3eb204d3367eaf97f03
Hasan-aga <careerforma@gmail.com>
2022-07-21 12:16:11 +0300

    include leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -14,7 +14,6 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(latitude, longitude);
