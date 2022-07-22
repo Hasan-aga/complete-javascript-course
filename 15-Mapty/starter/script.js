434baa57ecb9790b5c64d1fd7fa4ab21ed07f430
Hasan-aga <careerforma@gmail.com>
2022-07-22 23:29:25 +0300

    refactir event listener into class app

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-// localStorage.clear();
+localStorage.clear();
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -15,63 +15,6 @@ const statAverageDistance = document.querySelector('.avg__distance');
 const statAverageDuration = document.querySelector('.avg__duration');
 
 let currentWorkoutPosition, map;
-
-class App {
-  map;
-  constructor(user) {
-    this.user = user;
-  }
-  renderMap(latlng) {
-    this.map = L.map('map').setView(latlng, 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap',
-    }).addTo(this.map);
-
-    this.map.addPermenantMarker = function (latlng, popUp = false) {
-      const marker = L.marker(latlng).addTo(this);
-      if (popUp) {
-        marker.bindPopup(`${popUp}`).openPopup();
-      }
-      return this;
-    };
-
-    this.map.addTemporaryMarker = function (latlng) {
-      // if map already has marker -> move it to new coordinates
-      if (this.temporaryWorkoutMarker) {
-        this.temporaryWorkoutMarker.setLatLng(latlng);
-        return this;
-      }
-      this.temporaryWorkoutMarker = L.marker(latlng).addTo(this);
-      return this;
-    };
-
-    return this.map;
-  }
-
-  centerMapOn(latlng) {
-    this.map.setView(latlng, 13);
-  }
-
-  addMarkerForEachWorkout() {
-    this.user.workouts.forEach(workout =>
-      this.map.addPermenantMarker(workout.latlng)
-    );
-  }
-
-  displayInputForm() {
-    form.classList.remove('hidden');
-  }
-
-  clearForm() {
-    inputDistance.value =
-      inputCadence.value =
-      inputDuration.value =
-      inputElevation.value =
-        '';
-  }
-}
 
 class User {
   workouts = [];
@@ -152,6 +95,140 @@ class User {
 
   get lastWorkout() {
     return this.workouts.at(-1);
+  }
+}
+class App {
+  map;
+  user;
+  currentWorkoutPosition;
+  constructor() {
+    this.getPosition();
+
+    //   handle submit form
+    form.addEventListener('submit', this.handleSubmit.bind(this));
+
+    //toggle cadence and elevation by workout type
+    inputType.addEventListener('change', this.toggleFormWithType);
+
+    //   handle click on workout list
+    containerWorkouts.addEventListener(
+      'click',
+      this.handleClickOnWorkoutList.bind(this)
+    );
+  }
+
+  handleClickOnWorkoutList(event) {
+    const clicked = event.target;
+    if (clicked.classList.contains('workout__remove')) {
+      // handle click on remove button
+      const workoutElement = event.target.closest('.workout');
+      const workoutID = workoutElement.dataset.id;
+      const workoutToRemove = this.user.getWorkoutFromId(workoutID);
+      this.user.removeWorkout(workoutToRemove);
+    } else if (clicked.classList.contains('reset-storage')) {
+      console.log('reset local storage');
+      localStorage.clear();
+    } else if (!clicked || /form/gm.test(clicked.classList))
+      return; //handle click on empty section or input form
+    else {
+      //handle click on workout
+      const workoutElement = event.target.closest('.workout');
+      const workoutID = workoutElement.dataset.id;
+      const clickedWorkoutPosition =
+        this.user.getWorkoutFromId(workoutID).latlng;
+      this.centerMapOn(clickedWorkoutPosition);
+    }
+  }
+
+  toggleFormWithType() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.user.addWorkout(inputType.value, this.currentWorkoutPosition);
+    this.map.addPermenantMarker(
+      this.user.lastWorkout.latlng,
+      `<b>${this.user.lastWorkout.type}</b> <br> ${this.user.lastWorkout.shortDate}`
+    );
+    this.user.displayWorkoutsAndStats();
+    this.clearForm();
+  }
+
+  getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.initiateMap.bind(this),
+        alert('please provide position.')
+      );
+  }
+
+  initiateMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    this.user = new User(latitude, longitude);
+    console.log(this.user);
+    this.user.getStoredWorkouts();
+    this.user.displayAllWorkouts();
+    this.renderMap(this.user.latlng);
+    this.map.addPermenantMarker(this.user.latlng, '<b>This is you</b>');
+    this.addMarkerForEachWorkout();
+    this.map.on('click', this.handleMapClick.bind(this));
+  }
+
+  handleMapClick(event) {
+    form.classList.remove('hidden');
+    const latlng = Object.values(event.latlng);
+    this.currentWorkoutPosition = latlng;
+    this.map.addTemporaryMarker(this.currentWorkoutPosition);
+  }
+
+  renderMap(latlng) {
+    this.map = L.map('map').setView(latlng, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(this.map);
+
+    this.map.addPermenantMarker = function (latlng, popUp = false) {
+      const marker = L.marker(latlng).addTo(this);
+      if (popUp) {
+        marker.bindPopup(`${popUp}`).openPopup();
+      }
+      return this;
+    };
+
+    this.map.addTemporaryMarker = function (latlng) {
+      // if map already has marker -> move it to new coordinates
+      if (this.temporaryWorkoutMarker) {
+        this.temporaryWorkoutMarker.setLatLng(latlng);
+        return this;
+      }
+      this.temporaryWorkoutMarker = L.marker(latlng).addTo(this);
+      return this;
+    };
+
+    return this.map;
+  }
+
+  centerMapOn(latlng) {
+    this.map.setView(latlng, 13);
+  }
+
+  addMarkerForEachWorkout() {
+    this.user.workouts.forEach(workout =>
+      this.map.addPermenantMarker(workout.latlng)
+    );
+  }
+
+  clearForm() {
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
   }
 }
 
@@ -293,74 +370,4 @@ class Running extends Workout {
   }
 }
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const user = new User(latitude, longitude);
-      const app = new App(user);
-      user.getStoredWorkouts();
-      user.displayAllWorkouts();
-      app.renderMap(user.latlng);
-      app.map.addPermenantMarker(user.latlng, '<b>This is you</b>');
-      app.addMarkerForEachWorkout();
-      app.map.on('click', function (event) {
-        app.displayInputForm();
-        const latlng = Object.values(event.latlng);
-        currentWorkoutPosition = latlng;
-        app.map.addTemporaryMarker(currentWorkoutPosition);
-      });
-
-      //   handle submit form
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        user.addWorkout(inputType.value, currentWorkoutPosition);
-        app.map.addPermenantMarker(
-          user.lastWorkout.latlng,
-          `<b>${user.lastWorkout.type}</b> <br> ${user.lastWorkout.shortDate}`
-        );
-        user.displayWorkoutsAndStats();
-        app.clearForm();
-      });
-
-      //toggle cadence and elevation by workout type
-      inputType.addEventListener('change', function (event) {
-        inputElevation
-          .closest('.form__row')
-          .classList.toggle('form__row--hidden');
-        inputCadence
-          .closest('.form__row')
-          .classList.toggle('form__row--hidden');
-      });
-
-      //   handle click on workout list
-      containerWorkouts.addEventListener('click', function (event) {
-        const clicked = event.target;
-        if (clicked.classList.contains('workout__remove')) {
-          // handle click on remove button
-          const workoutElement = event.target.closest('.workout');
-          const workoutID = workoutElement.dataset.id;
-          const workoutToRemove = user.getWorkoutFromId(workoutID);
-          user.removeWorkout(workoutToRemove);
-        } else if (clicked.classList.contains('reset-storage')) {
-          console.log('reset local storage');
-          localStorage.clear();
-        } else if (!clicked || /form/gm.test(clicked.classList))
-          return; //handle click on empty section or input form
-        else {
-          //handle click on workout
-          const workoutElement = event.target.closest('.workout');
-          const workoutID = workoutElement.dataset.id;
-          const clickedWorkoutPosition =
-            user.getWorkoutFromId(workoutID).latlng;
-          app.centerMapOn(clickedWorkoutPosition);
-        }
-      });
-
-      //   TODO: handle creating workout without clicking on map
-    },
-    function () {
-      alert('Could not get position.');
-    }
-  );
+const app = new App();
