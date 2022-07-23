59910fc8aa4153196408a2e7a430a9e606365974
Hasan-aga <careerforma@gmail.com>
2022-07-23 21:36:11 +0300

    remove marker from workout circular object when stringify

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -109,11 +109,10 @@ class App {
   handleSubmit(event) {
     event.preventDefault();
     this.user.addWorkout(inputType.value, this.currentWorkoutPosition);
-    const marker = this.map.addPermenantMarker(
+    this.map.addPermenantMarker(
       this.user.lastWorkout.latlng,
       `<b>${this.user.lastWorkout.type}</b> <br> ${this.user.lastWorkout.shortDate}`
     );
-    this.user.lastWorkout.marker = marker;
     console.log(this.user.lastWorkout);
     this.user.displayWorkoutsAndStats();
     form.classList.add('hidden');
--- 15-Mapty/starter/workoutClass.js
+++ 15-Mapty/starter/workoutClass.js
@@ -12,7 +12,6 @@ import {
 } from './DOMelements.js';
 
 export class Workout {
-  marker;
   timeStamp;
   shortDate;
   options = {
