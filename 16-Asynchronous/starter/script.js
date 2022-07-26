bde04766de4f93f342d9f600a4b828b4ae3d1462
Hasan-aga <careerforma@gmail.com>
2022-07-26 14:07:27 +0300

    add a country the old way

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,36 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const country = 'iraq';
+
+function addCountry(country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function (e) {
+    const [country] = JSON.parse(this.responseText);
+    const countryHtmlCode = `<article class="country">
+        <img class="country__img" src="${country.flags.svg}" />
+        <div class="country__data">
+          <h3 class="country__name">${country.name.common}</h3>
+          <h4 class="country__region">${country.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            country.population / 1000_000
+          ).toFixed(0)} million people</p>
+          <p class="country__row"><span>🗣️</span>${
+            Object.values(country.languages)[0]
+          }</p>
+          <p class="country__row"><span>💰</span>${Object.keys(
+            country.currencies
+          )}</p>
+        </div>
+      </article>
+      `;
+
+    countriesContainer.insertAdjacentHTML('afterbegin', countryHtmlCode);
+    countriesContainer.style.opacity = 1;
+  });
+}
+
+addCountry('israel');
+addCountry('egypt');
