c709e2049966c49e3c989dc60c1ffe599df4cf84
Hasan-aga <careerforma@gmail.com>
2022-07-26 21:07:30 +0300

    solved first challenge

--- 16-Asynchronous/starter/challenge1.js
+++ 16-Asynchronous/starter/challenge1.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function whereAmI(lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (res.ok) return res.json();
+      throw new Error(`Could not make api call, status: ${res.status}`);
+    })
+    .then(data => console.log(`You are in ${data.city} / ${data.country}`))
+    .catch(err => console.error('Failed!', err.message));
+}
+
+whereAmI(52.508, 13.381);
