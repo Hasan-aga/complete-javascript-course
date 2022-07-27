badfee5cb7a6a6451b8d996b974c6b15c661c98d
Hasan-aga <careerforma@gmail.com>
2022-07-27 10:34:45 +0300

    demo of event loop and queues

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -37,45 +37,48 @@ const countriesContainer = document.querySelector('.countries');
 
 // addCountry('france');
 
-function createCountryElement(country, className = '') {
-  const countryHtmlCode = `<article class="country ${className}">
-            <img class="country__img" src="${country.flags.svg}" />
-            <div class="country__data">
-          <h3 class="country__name">${country.name.common}</h3>
-          <h4 class="country__region">${country.region}</h4>
-          <p class="country__row"><span>👫</span>${(
-            country.population / 1000_000
-          ).toFixed(0)} million people</p>
-          <p class="country__row"><span>🗣️</span>${
-            Object.values(country.languages)[0]
-          }</p>
-          <p class="country__row"><span>💰</span>${Object.keys(
-            country.currencies
-          )}</p>
-        </div>
-      </article>
-      `;
+// function createCountryElement(country, className = '') {
+//   const countryHtmlCode = `<article class="country ${className}">
+//             <img class="country__img" src="${country.flags.svg}" />
+//             <div class="country__data">
+//           <h3 class="country__name">${country.name.common}</h3>
+//           <h4 class="country__region">${country.region}</h4>
+//           <p class="country__row"><span>👫</span>${(
+//             country.population / 1000_000
+//           ).toFixed(0)} million people</p>
+//           <p class="country__row"><span>🗣️</span>${
+//             Object.values(country.languages)[0]
+//           }</p>
+//           <p class="country__row"><span>💰</span>${Object.keys(
+//             country.currencies
+//           )}</p>
+//         </div>
+//       </article>
+//       `;
 
-  countriesContainer.style.opacity = 1;
-  countriesContainer.insertAdjacentHTML('afterbegin', countryHtmlCode);
-  return country?.borders;
-}
+//   countriesContainer.style.opacity = 1;
+//   countriesContainer.insertAdjacentHTML('afterbegin', countryHtmlCode);
+//   return country?.borders;
+// }
 
-function getCountryData(response, className = '') {
-  if (!response.ok)
-    throw new Error(`no country was found! status: ${response.status}`);
+// function getCountryData(response, className = '') {
+//   if (!response.ok)
+//     throw new Error(`no country was found! status: ${response.status}`);
 
-  return response.json().then(data => createCountryElement(data[0], className));
-}
+//   return response.json().then(data => createCountryElement(data[0], className));
+// }
 
-function getCountryData(country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => getCountryData(response))
-    .then(borders =>
-      fetch(`https://restcountries.com/v3.1/alpha/${borders[0]}`)
-    )
-    .then(response => getCountryData(response))
-    .catch(error => console.log('FAILED!', error.message));
-}
+// function getCountryData(country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => getCountryData(response))
+//     .then(borders =>
+//       fetch(`https://restcountries.com/v3.1/alpha/${borders[0]}`)
+//     )
+//     .then(response => getCountryData(response))
+//     .catch(error => console.log('FAILED!', error.message));
+// }
 
-btn.addEventListener('click', () => getCountryData('turkey'));
+// btn.addEventListener('click', () => getCountryData('turkey'));
+
+// setTimeout(() => console.log('hello callback queue'), 0);
+// Promise.resolve('Hello microtask queue').then(res => console.log(res));
